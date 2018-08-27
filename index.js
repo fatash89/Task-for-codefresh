1c9f539b900db648390bf30473badd4e868ee198
denys <voznyukden23@gmail.com>
2018-08-27 17:27:54 +0300

    test

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const UsersSessionsController = require('./controllers/users.sessions.controller');
 app.get('/parseUsersSessions', UsersSessionsController.getUsersSessions);
-console.log('1');
+console.log('12');
+console.log('12');
 app.use(express.static('public'));
 
 /*error handler for log errors*/
