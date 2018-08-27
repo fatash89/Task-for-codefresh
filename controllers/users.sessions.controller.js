d7820dd42b9be29e0df6f41c881fba07ebc119b2
denys <voznyukden23@gmail.com>
2018-08-27 18:03:27 +0300

    test2

--- controllers/users.sessions.controller.js
+++ controllers/users.sessions.controller.js
@@ -1,5 +1,5 @@
 const UsersSessionsService = require('../services/users.sessions.service');
-console.log('1');
+
 class UsersSessionsController {
     static async getUsersSessions(req, res){
     	/*here we catch errors occurred while processing the request */
