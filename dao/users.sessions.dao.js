3fbdcfc2f5620bd57dcd125489c276a11de7f663
denys-codefresh <41425204+denys-codefresh@users.noreply.github.com>
2018-08-28 11:47:20 +0300

    Update users.sessions.dao.js

--- dao/users.sessions.dao.js
+++ dao/users.sessions.dao.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const readFileAsync = require('util').promisify(fs.readFile);
 const {createFileRowsStructure} = require('../helpers/users.session.helpers');
-
+//
 class UsersSessionsDao {
     static async getUsersSessions(){
         const readedBuffer = await readFileAsync('./airbnb_session_data.txt');
